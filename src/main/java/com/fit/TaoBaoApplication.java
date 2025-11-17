161f10353140bd8f9ec203b604268acb7e884392
cyril314 <svip.cyril@gmail.com>
2025-11-17 19:18:31 +0800

    Initial commit

--- src/main/java/com/fit/TaoBaoApplication.java
+++ src/main/java/com/fit/TaoBaoApplication.java
@@ -6,10 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.util.StringUtils;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-import java.util.Optional;
 
 @Slf4j
 @SpringBootApplication
@@ -17,19 +17,17 @@ public class TaoBaoApplication extends SpringBootServletInitializer {
     public static void main(String[] args) throws UnknownHostException {
         ConfigurableApplicationContext run = SpringApplication.run(TaoBaoApplication.class, args);
         String ip = InetAddress.getLocalHost().getHostAddress();
-        String port = getParam("POST", run.getEnvironment().getProperty("server.port"));
+        String port = System.getenv("POST");
+        if (StringUtils.isEmpty(port)) {
+            port = run.getEnvironment().getProperty("server.port");
+        }
         log.info("---------------------------------------------------------");
-        log.info("\nApplication AceAdmin is running! Access URLs:\n\tLocal: \t\thttp://localhost:{}\n\tExternal:\thttp://{}:{}", port, ip, port);
-        log.info("-----------------已启动部署 admin-web----------------------");
+        log.info("Access URLs:\n\tLocal: \t\thttp://localhost:{}/\n\tExternal:\t{}://{}:{}/", port, "http", ip, port);
+        log.info("---------------------- admin-web ------------------------");
     }
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(TaoBaoApplication.class);
-    }
-
-    private static String getParam(String param, String defVal) {
-        Optional<String> envPort = Optional.ofNullable(System.getenv(param));
-        return envPort.map(String::valueOf).orElse(defVal);
     }
 }
